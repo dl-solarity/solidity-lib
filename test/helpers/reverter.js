545b34115dd81cb3170d1812348f9a25ae08f3e3
Artem Chystiakov <artem.ch31@gmail.com>
2022-06-22 00:59:39 +0300

    simplified reverter

--- test/helpers/reverter.js
+++ test/helpers/reverter.js
@@ -3,32 +3,14 @@ const ht = require("hardhat");
 class Reverter {
   #snapshotId;
 
-  revert() {
-    return new Promise((resolve, reject) => {
-      ht.network.provider
-        .send("evm_revert", [this.#snapshotId])
-        .then(() => {
-          return resolve(this.snapshot());
-        })
-        .catch((err) => {
-          return reject(err);
-        });
-    });
-  }
+  revert = async () => {
+    await ht.network.provider.send("evm_revert", [this.#snapshotId]);
+    await this.snapshot();
+  };
 
-  snapshot() {
-    return new Promise((resolve, reject) => {
-      ht.network.provider
-        .send("evm_snapshot", [])
-        .then((res) => {
-          this.#snapshotId = res;
-          return resolve(res);
-        })
-        .catch((err) => {
-          return reject(err);
-        });
-    });
-  }
+  snapshot = async () => {
+    this.#snapshotId = await ht.network.provider.send("evm_snapshot", []);
+  };
 }
 
 module.exports = Reverter;
