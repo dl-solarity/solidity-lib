5f5b967e0691eeb9eab6a85cd4f44ec10adc90fb
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-09-13 18:54:30 +0300

    added globals

---
+++ contracts/utils/Globals.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Globals {
+    uint256 public constant PRECISION = 10**25;
+    uint256 public constant DECIMAL = 10**18;
+    uint256 public constant PERCENTAGE_100 = 10**27;
+}
