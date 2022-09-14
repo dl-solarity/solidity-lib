c4b59d606d05453671818aec17dac205f1863356
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-09-14 18:53:05 +0300

    rm lib

--- contracts/utils/Globals.sol
+++ contracts/utils/Globals.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-library Globals {
-    uint256 public constant PRECISION = 10**25;
-    uint256 public constant DECIMAL = 10**18;
-    uint256 public constant PERCENTAGE_100 = 10**27;
-}
+uint256 constant PRECISION = 10**25;
+uint256 constant DECIMAL = 10**18;
+uint256 constant PERCENTAGE_100 = 10**27;
