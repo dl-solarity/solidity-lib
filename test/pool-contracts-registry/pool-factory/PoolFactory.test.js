0d8734d09c9acf379fc63a1f4974664502f2e2f9
dovgopoly <idovgopoly@gmail.com>
2023-01-06 13:19:50 +0200

    refactored tests

--- test/pool-contracts-registry/pool-factory/PoolFactory.test.js
+++ test/pool-contracts-registry/pool-factory/PoolFactory.test.js
@@ -66,7 +66,7 @@ describe("PoolFactory", () => {
     });
   });
 
-  describe("deploy()", () => {
+  describe("after setting the implementation", () => {
     let poolImpl;
 
     beforeEach("setup", async () => {
@@ -75,100 +75,93 @@ describe("PoolFactory", () => {
       await poolContractsRegistry.setNewImplementations([NAME_1], [poolImpl.address]);
     });
 
-    it("should deploy pool", async () => {
-      await poolFactory.deployPool();
+    describe("deploy()", () => {
+      it("should deploy pool", async () => {
+        await poolFactory.deployPool();
 
-      assert.equal(toBN(await poolContractsRegistry.countPools(NAME_1)).toFixed(), "1");
-      assert.equal(toBN(await poolContractsRegistry.countPools(NAME_2)).toFixed(), "0");
+        assert.equal(toBN(await poolContractsRegistry.countPools(NAME_1)).toFixed(), "1");
+        assert.equal(toBN(await poolContractsRegistry.countPools(NAME_2)).toFixed(), "0");
 
-      const pool = await Pool.at((await poolContractsRegistry.listPools(NAME_1, 0, 1))[0]);
-      const beaconProxy = await BeaconProxy.at(pool.address);
+        const pool = await Pool.at((await poolContractsRegistry.listPools(NAME_1, 0, 1))[0]);
+        const beaconProxy = await BeaconProxy.at(pool.address);
 
-      assert.equal(await beaconProxy.implementation(), poolImpl.address);
-      assert.notEqual(await pool.token(), ZERO);
+        assert.equal(await beaconProxy.implementation(), poolImpl.address);
+        assert.notEqual(await pool.token(), ZERO);
+      });
+
+      it("should not register pools", async () => {
+        await contractsRegistry.addContract(await contractsRegistry.POOL_FACTORY_NAME(), OWNER);
+        await contractsRegistry.injectDependencies(await contractsRegistry.POOL_CONTRACTS_REGISTRY_NAME());
+
+        await truffleAssert.reverts(poolFactory.deployPool(), "AbstractPoolFactory: failed to register contract");
+      });
+
+      it("should deploy several pools", async () => {
+        await poolFactory.deployPool();
+        await poolFactory.deployPool();
+        await poolFactory.deployPool();
+        assert.equal(toBN(await poolContractsRegistry.countPools(NAME_1)).toFixed(), "3");
+      });
+
+      it("should set access correctly", async () => {
+        await poolFactory.deployPool();
+
+        const pool = await Pool.at((await poolContractsRegistry.listPools(NAME_1, 0, 1))[0]);
+
+        await truffleAssert.reverts(
+          poolContractsRegistry.addProxyPool(NAME_1, poolFactory.address),
+          "PoolContractsRegistry: not a factory"
+        );
+        await truffleAssert.reverts(pool.setDependencies(contractsRegistry.address), "Dependant: Not an injector");
+      });
+
+      it("should upgrade pools", async () => {
+        await poolFactory.deployPool();
+
+        const pool1 = await PoolUpgrade.at((await poolContractsRegistry.listPools(NAME_1, 0, 1))[0]);
+        await truffleAssert.reverts(pool1.addedFunction());
+
+        const poolUpgrade = await PoolUpgrade.new();
+        await poolContractsRegistry.setNewImplementations([NAME_1], [poolUpgrade.address]);
+
+        assert.equal(toBN(await pool1.addedFunction()).toFixed(), "42");
+
+        await poolFactory.deployPool();
+
+        const pool2 = await PoolUpgrade.at((await poolContractsRegistry.listPools(NAME_1, 1, 1))[0]);
+        assert.equal(toBN(await pool2.addedFunction()).toFixed(), "42");
+      });
     });
 
-    it("should not register pools", async () => {
-      await contractsRegistry.addContract(await contractsRegistry.POOL_FACTORY_NAME(), OWNER);
-      await contractsRegistry.injectDependencies(await contractsRegistry.POOL_CONTRACTS_REGISTRY_NAME());
-
-      await truffleAssert.reverts(poolFactory.deployPool(), "AbstractPoolFactory: failed to register contract");
-    });
-
-    it("should deploy several pools", async () => {
-      await poolFactory.deployPool();
-      await poolFactory.deployPool();
-      await poolFactory.deployPool();
-
-      assert.equal(toBN(await poolContractsRegistry.countPools(NAME_1)).toFixed(), "3");
-    });
-
-    it("should set access correctly", async () => {
-      await poolFactory.deployPool();
-
-      const pool = await Pool.at((await poolContractsRegistry.listPools(NAME_1, 0, 1))[0]);
-
-      await truffleAssert.reverts(
-        poolContractsRegistry.addProxyPool(NAME_1, poolFactory.address),
-        "PoolContractsRegistry: not a factory"
-      );
-      await truffleAssert.reverts(pool.setDependencies(contractsRegistry.address), "Dependant: Not an injector");
-    });
-
-    it("should upgrade pools", async () => {
-      await poolFactory.deployPool();
-
-      const pool1 = await PoolUpgrade.at((await poolContractsRegistry.listPools(NAME_1, 0, 1))[0]);
-      await truffleAssert.reverts(pool1.addedFunction());
-
-      const poolUpgrade = await PoolUpgrade.new();
-      await poolContractsRegistry.setNewImplementations([NAME_1], [poolUpgrade.address]);
-
-      assert.equal(toBN(await pool1.addedFunction()).toFixed(), "42");
-
-      await poolFactory.deployPool();
-
-      const pool2 = await PoolUpgrade.at((await poolContractsRegistry.listPools(NAME_1, 1, 1))[0]);
-      assert.equal(toBN(await pool2.addedFunction()).toFixed(), "42");
-    });
-  });
-
-  describe("deploy2() predictPoolAddress()", () => {
-    let poolImpl;
-
-    beforeEach("setup", async () => {
-      poolImpl = await Pool.new();
-
-      await poolContractsRegistry.setNewImplementations([NAME_1], [poolImpl.address]);
-    });
-
-    it("should deploy on the predicted address", async () => {
+    describe("deploy2() predictPoolAddress()", () => {
       const SALT1 = "pool_salt1";
       const SALT2 = "pool_salt2";
 
-      const predictedAddress1 = await poolFactory.predictPoolAddress(SALT1);
-      const predictedAddress2 = await poolFactory.predictPoolAddress(SALT2);
+      it("should deploy on the predicted address", async () => {
+        const predictedAddress1 = await poolFactory.predictPoolAddress(SALT1);
+        const predictedAddress2 = await poolFactory.predictPoolAddress(SALT2);
 
-      await poolFactory.deploy2Pool(SALT1);
-      await poolFactory.deploy2Pool(SALT2);
+        await poolFactory.deploy2Pool(SALT1);
+        await poolFactory.deploy2Pool(SALT2);
 
-      assert.equal(toBN(await poolContractsRegistry.countPools(NAME_1)).toFixed(), "2");
-      assert.equal(toBN(await poolContractsRegistry.countPools(NAME_2)).toFixed(), "0");
+        assert.equal(toBN(await poolContractsRegistry.countPools(NAME_1)).toFixed(), "2");
+        assert.equal(toBN(await poolContractsRegistry.countPools(NAME_2)).toFixed(), "0");
 
-      const pools = await poolContractsRegistry.listPools(NAME_1, 0, 2);
+        const pools = await poolContractsRegistry.listPools(NAME_1, 0, 2);
 
-      assert.deepEqual(pools, [predictedAddress1, predictedAddress2]);
+        assert.deepEqual(pools, [predictedAddress1, predictedAddress2]);
 
-      const poolProxies = await Promise.all(pools.map(async (pool) => await Pool.at(pool)));
-      const beaconProxies = await Promise.all(pools.map(async (pool) => await BeaconProxy.at(pool)));
+        const poolProxies = await Promise.all(pools.map(async (pool) => await Pool.at(pool)));
+        const beaconProxies = await Promise.all(pools.map(async (pool) => await BeaconProxy.at(pool)));
 
-      const tokens = await Promise.all(poolProxies.map(async (poolContract) => await poolContract.token()));
-      const implementations = await Promise.all(
-        beaconProxies.map(async (beaconProxy) => await beaconProxy.implementation())
-      );
+        const tokens = await Promise.all(poolProxies.map(async (poolProxy) => await poolProxy.token()));
+        const implementations = await Promise.all(
+          beaconProxies.map(async (beaconProxy) => await beaconProxy.implementation())
+        );
 
-      assert.deepEqual(tokens, [token.address, token.address]);
-      assert.deepEqual(implementations, [poolImpl.address, poolImpl.address]);
+        assert.deepEqual(tokens, [token.address, token.address]);
+        assert.deepEqual(implementations, [poolImpl.address, poolImpl.address]);
+      });
     });
   });
 });
