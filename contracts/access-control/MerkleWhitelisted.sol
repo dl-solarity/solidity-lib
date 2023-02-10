ec378c1456c49c09bffc2d6a70678ef416590433
dovgopoly <idovgopoly@gmail.com>
2023-02-10 19:51:28 +0200

    code-style & changed contract name

--- contracts/access-control/MerkleWhitelisted.sol
+++ contracts/access-control/MerkleWhitelisted.sol
@@ -3,23 +3,19 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-abstract contract MerkleWhitelist {
+abstract contract MerkleWhitelisted {
     using MerkleProof for bytes32[];
 
     bytes32 internal _merkleRoot;
 
     modifier onlyWhitelisted(bytes32[] calldata merkleProof_) {
-        require(isWhitelisted(msg.sender, merkleProof_), "MerkleWhitelist: not whitelisted");
+        require(isWhitelisted(msg.sender, merkleProof_), "MerkleWhitelisted: not whitelisted");
         _;
     }
 
     modifier onlyWhitelistedUser(address user_, bytes32[] calldata merkleProof_) {
-        require(isWhitelisted(user_, merkleProof_), "MerkleWhitelist: not whitelisted");
+        require(isWhitelisted(user_, merkleProof_), "MerkleWhitelisted: not whitelisted");
         _;
-    }
-
-    function setMerkleRoot(bytes32 merkleRoot_) internal {
-        _merkleRoot = merkleRoot_;
     }
 
     function isWhitelisted(
@@ -27,5 +23,9 @@ abstract contract MerkleWhitelist {
         bytes32[] calldata merkleProof_
     ) public view returns (bool) {
         return merkleProof_.verifyCalldata(_merkleRoot, keccak256(abi.encodePacked(user_)));
+    }
+
+    function _setMerkleRoot(bytes32 merkleRoot_) internal {
+        _merkleRoot = merkleRoot_;
     }
 }
