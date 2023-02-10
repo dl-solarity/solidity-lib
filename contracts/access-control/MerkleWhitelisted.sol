772e126c4a3d685b49d29c97e17a8ee06588baf3
dovgopoly <idovgopoly@gmail.com>
2023-02-10 20:24:26 +0200

    refactored & added methods

--- contracts/access-control/MerkleWhitelisted.sol
+++ contracts/access-control/MerkleWhitelisted.sol
@@ -6,9 +6,9 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 abstract contract MerkleWhitelisted {
     using MerkleProof for bytes32[];
 
-    bytes32 internal _merkleRoot;
+    bytes32 private _merkleRoot;
 
-    modifier onlyWhitelisted(bytes calldata data_, bytes32[] calldata merkleProof_) {
+    modifier onlyWhitelisted(bytes memory data_, bytes32[] calldata merkleProof_) {
         require(
             isWhitelisted(keccak256(data_), merkleProof_),
             "MerkleWhitelisted: not whitelisted"
@@ -18,7 +18,7 @@ abstract contract MerkleWhitelisted {
 
     modifier onlyWhitelistedUser(address user_, bytes32[] calldata merkleProof_) {
         require(
-            isWhitelisted(keccak256(abi.encodePacked(user_)), merkleProof_),
+            isWhitelisted(_convertUserToLeaf(user_), merkleProof_),
             "MerkleWhitelisted: not whitelisted"
         );
         _;
@@ -31,7 +31,22 @@ abstract contract MerkleWhitelisted {
         return merkleProof_.verifyCalldata(_merkleRoot, leaf_);
     }
 
+    function isWhitelistedUser(
+        address user_,
+        bytes32[] calldata merkleProof_
+    ) public view returns (bool) {
+        return merkleProof_.verifyCalldata(_merkleRoot, _convertUserToLeaf(user_));
+    }
+
+    function getMerkleRoot() public view returns (bytes32) {
+        return _merkleRoot;
+    }
+
     function _setMerkleRoot(bytes32 merkleRoot_) internal {
         _merkleRoot = merkleRoot_;
+    }
+
+    function _convertUserToLeaf(address user_) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(user_));
     }
 }
