ed863212720178f31bcb06ea4d0ab3108f266e73
dovgopoly <idovgopoly@gmail.com>
2023-02-11 18:35:18 +0200

    added ZERO_BYTES32 constant & refactored mock contract & coverage 100%

--- contracts/mock/access-control/MerkleWhitelistedMock.sol
+++ contracts/mock/access-control/MerkleWhitelistedMock.sol
@@ -4,16 +4,27 @@ pragma solidity ^0.8.4;
 import "../../access-control/MerkleWhitelisted.sol";
 
 contract MerkleWhitelistedMock is MerkleWhitelisted {
+    event WhitelistedUser();
+    event WhitelistedData();
+
     function onlyWhitelistedMethod(
-        bytes calldata data_,
+        uint256 amount_,
         bytes32[] calldata merkleProof_
-    ) external onlyWhitelisted(data_, merkleProof_) {}
+    ) external onlyWhitelisted(_encode(amount_), merkleProof_) {
+        emit WhitelistedData();
+    }
 
     function onlyWhitelistedUserMethod(
         bytes32[] calldata merkleProof_
-    ) external onlyWhitelistedUser(msg.sender, merkleProof_) {}
+    ) external onlyWhitelistedUser(msg.sender, merkleProof_) {
+        emit WhitelistedUser();
+    }
 
     function setMerkleRoot(bytes32 merkleRoot_) external {
         _setMerkleRoot(merkleRoot_);
+    }
+
+    function _encode(uint256 amount_) private view returns (bytes memory) {
+        return abi.encodePacked(amount_, msg.sender);
     }
 }
