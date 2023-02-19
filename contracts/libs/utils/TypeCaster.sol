ae80f5cccdb5b8d37cead1312e6dd707759f9779
dovgopoly <idovgopoly@gmail.com>
2023-02-19 15:58:37 +0200

    refactored sol part

--- contracts/libs/utils/TypeCaster.sol
+++ contracts/libs/utils/TypeCaster.sol
@@ -2,54 +2,11 @@
 pragma solidity ^0.8.4;
 
 library TypeCaster {
-    function asBytes(bool from) internal pure returns (bytes memory) {
-        return abi.encode(from);
-    }
-
-    function asBytes(address from) internal pure returns (bytes memory) {
-        return abi.encode(from);
-    }
-
-    function asBytes(uint256 from) internal pure returns (bytes memory) {
-        return abi.encode(from);
-    }
-
-    function asBytes(bytes32 from) internal pure returns (bytes memory) {
-        return abi.encode(from);
-    }
-
-    function asBytes(string memory from) internal pure returns (bytes memory) {
-        return abi.encode(from);
-    }
-
-    function asBytes(bytes32[] memory from) internal pure returns (bytes memory) {
-        return abi.encode(from);
-    }
-
-    function asBool(bytes memory from) internal pure returns (bool) {
-        return abi.decode(from, (bool));
-    }
-
-    function asAddress(bytes memory from) internal pure returns (address) {
-        return abi.decode(from, (address));
-    }
-
-    function asBytes32(bytes memory from) internal pure returns (bytes32) {
-        return abi.decode(from, (bytes32));
-    }
-
-    function asUint256(bytes memory from) internal pure returns (uint256) {
-        return abi.decode(from, (uint256));
-    }
-
-    function asString(bytes memory from) internal pure returns (string memory) {
-        return abi.decode(from, (string));
-    }
-
-    function asBytes32Array(bytes memory from) internal pure returns (bytes32[] memory) {
-        return abi.decode(from, (bytes32[]));
-    }
-
+    /**
+     *  @notice The function that casts the list of `X` types to the list of `Y` types
+     *  @param from the list of `X` types
+     *  @return array_ the list of `Y` types
+     */
     function asUint256Array(
         bytes32[] memory from
     ) internal pure returns (uint256[] memory array_) {
@@ -58,8 +15,24 @@ library TypeCaster {
         }
     }
 
+    function asUint256Array(
+        address[] memory from
+    ) internal pure returns (uint256[] memory array_) {
+        assembly {
+            array_ := from
+        }
+    }
+
     function asAddressArray(
         bytes32[] memory from
+    ) internal pure returns (address[] memory array_) {
+        assembly {
+            array_ := from
+        }
+    }
+
+    function asAddressArray(
+        uint256[] memory from
     ) internal pure returns (address[] memory array_) {
         assembly {
             array_ := from
@@ -82,6 +55,11 @@ library TypeCaster {
         }
     }
 
+    /**
+     *  @notice The function to transform an element into an array
+     *  @param from the element
+     *  @return array_ the element as an array
+     */
     function asSingletonArray(uint256 from) internal pure returns (uint256[] memory array_) {
         array_ = new uint256[](1);
         array_[0] = from;
@@ -94,6 +72,11 @@ library TypeCaster {
 
     function asSingletonArray(string memory from) internal pure returns (string[] memory array_) {
         array_ = new string[](1);
+        array_[0] = from;
+    }
+
+    function asSingletonArray(bytes32 from) internal pure returns (bytes32[] memory array_) {
+        array_ = new bytes32[](1);
         array_[0] = from;
     }
 }
--- contracts/mock/libs/utils/TypeCasterMock.sol
+++ contracts/mock/libs/utils/TypeCasterMock.sol
@@ -6,87 +6,57 @@ import "../../../libs/utils/TypeCaster.sol";
 contract TypeCasterMock {
     using TypeCaster for *;
 
-    function asBytes_Bool(bool from_) external pure returns (bytes memory) {
-        return from_.asBytes();
-    }
-
-    function asBytes_Address(address from_) external pure returns (bytes memory) {
-        return from_.asBytes();
-    }
-
-    function asBytes_Uint256(uint256 from_) external pure returns (bytes memory) {
-        return from_.asBytes();
-    }
-
-    function asBytes_Bytes32(bytes32 from_) external pure returns (bytes memory) {
-        return from_.asBytes();
-    }
-
-    function asBytes_String(string memory from_) external pure returns (bytes memory) {
-        return from_.asBytes();
-    }
-
-    function asBytes_Bytes32Array(bytes32[] memory from_) external pure returns (bytes memory) {
-        return from_.asBytes();
-    }
-
-    function asBool_Bytes(bytes memory from_) external pure returns (bool) {
-        return from_.asBool();
-    }
-
-    function asAddress_Bytes(bytes memory from_) external pure returns (address) {
-        return from_.asAddress();
-    }
-
-    function asBytes32_Bytes(bytes memory from_) external pure returns (bytes32) {
-        return from_.asBytes32();
-    }
-
-    function asUint256_Bytes(bytes memory from_) external pure returns (uint256) {
-        return from_.asUint256();
-    }
-
-    function asString_Bytes(bytes memory from_) external pure returns (string memory) {
-        return from_.asString();
-    }
-
-    function asBytes32Array_Bytes(bytes memory from_) external pure returns (bytes32[] memory) {
-        return from_.asBytes32Array();
-    }
-
-    function asUint256Array_Bytes32Array(
+    function asUint256ArrayFromBytes32Array(
         bytes32[] memory from_
     ) external pure returns (uint256[] memory) {
         return from_.asUint256Array();
     }
 
-    function asAddressArray_Bytes32Array(
+    function asUint256ArrayFromAddressArray(
+        address[] memory from_
+    ) external pure returns (uint256[] memory) {
+        return from_.asUint256Array();
+    }
+
+    function asAddressArrayFromBytes32Array(
         bytes32[] memory from_
     ) external pure returns (address[] memory) {
         return from_.asAddressArray();
     }
 
-    function asBytes32Array_Uint256Array(
+    function asAddressArrayFromUint256Array(
+        uint256[] memory from_
+    ) external pure returns (address[] memory) {
+        return from_.asAddressArray();
+    }
+
+    function asBytes32ArrayFromUint256Array(
         uint256[] memory from_
     ) external pure returns (bytes32[] memory) {
         return from_.asBytes32Array();
     }
 
-    function asBytes32Array_AddressArray(
+    function asBytes32ArrayFromAddressArray(
         address[] memory from_
     ) external pure returns (bytes32[] memory) {
         return from_.asBytes32Array();
     }
 
-    function asSingletonArray_Uint256(uint256 from_) external pure returns (uint256[] memory) {
+    function asSingletonArrayFromUint256(uint256 from_) external pure returns (uint256[] memory) {
         return from_.asSingletonArray();
     }
 
-    function asSingletonArray_Address(address from_) external pure returns (address[] memory) {
+    function asSingletonArrayFromAddress(address from_) external pure returns (address[] memory) {
         return from_.asSingletonArray();
     }
 
-    function asSingletonArray_String(string memory from_) external pure returns (string[] memory) {
+    function asSingletonArrayFromString(
+        string memory from_
+    ) external pure returns (string[] memory) {
+        return from_.asSingletonArray();
+    }
+
+    function asSingletonArrayFromBytes32(bytes32 from_) external pure returns (bytes32[] memory) {
         return from_.asSingletonArray();
     }
 }
