b33957fb4198292309317c782b05eb89b99cbe65
dovgopoly <idovgopoly@gmail.com>
2023-02-20 13:22:44 +0200

    fixed codestyle

--- contracts/libs/utils/TypeCaster.sol
+++ contracts/libs/utils/TypeCaster.sol
@@ -7,89 +7,89 @@ pragma solidity ^0.8.4;
 library TypeCaster {
     /**
      *  @notice The function that casts the list of `X`-type elements to the list of uint256
-     *  @param from the list of `X`-type elements
+     *  @param from_ the list of `X`-type elements
      *  @return array_ the list of uint256
      */
     function asUint256Array(
-        bytes32[] memory from
+        bytes32[] memory from_
     ) internal pure returns (uint256[] memory array_) {
         assembly {
-            array_ := from
+            array_ := from_
         }
     }
 
     function asUint256Array(
-        address[] memory from
+        address[] memory from_
     ) internal pure returns (uint256[] memory array_) {
         assembly {
-            array_ := from
+            array_ := from_
         }
     }
 
     /**
      *  @notice The function that casts the list of `X`-type elements to the list of addresses
-     *  @param from the list of `X`-type elements
+     *  @param from_ the list of `X`-type elements
      *  @return array_ the list of addresses
      */
     function asAddressArray(
-        bytes32[] memory from
+        bytes32[] memory from_
     ) internal pure returns (address[] memory array_) {
         assembly {
-            array_ := from
+            array_ := from_
         }
     }
 
     function asAddressArray(
-        uint256[] memory from
+        uint256[] memory from_
     ) internal pure returns (address[] memory array_) {
         assembly {
-            array_ := from
+            array_ := from_
         }
     }
 
     /**
      *  @notice The function that casts the list of `X`-type elements to the list of bytes32
-     *  @param from the list of `X`-type elements
+     *  @param from_ the list of `X`-type elements
      *  @return array_ the list of bytes32
      */
     function asBytes32Array(
-        uint256[] memory from
+        uint256[] memory from_
     ) internal pure returns (bytes32[] memory array_) {
         assembly {
-            array_ := from
+            array_ := from_
         }
     }
 
     function asBytes32Array(
-        address[] memory from
+        address[] memory from_
     ) internal pure returns (bytes32[] memory array_) {
         assembly {
-            array_ := from
+            array_ := from_
         }
     }
 
     /**
      *  @notice The function to transform an element into an array
-     *  @param from the element
+     *  @param from_ the element
      *  @return array_ the element as an array
      */
-    function asSingletonArray(uint256 from) internal pure returns (uint256[] memory array_) {
+    function asSingletonArray(uint256 from_) internal pure returns (uint256[] memory array_) {
         array_ = new uint256[](1);
-        array_[0] = from;
+        array_[0] = from_;
     }
 
-    function asSingletonArray(address from) internal pure returns (address[] memory array_) {
+    function asSingletonArray(address from_) internal pure returns (address[] memory array_) {
         array_ = new address[](1);
-        array_[0] = from;
+        array_[0] = from_;
     }
 
-    function asSingletonArray(string memory from) internal pure returns (string[] memory array_) {
+    function asSingletonArray(string memory from_) internal pure returns (string[] memory array_) {
         array_ = new string[](1);
-        array_[0] = from;
+        array_[0] = from_;
     }
 
-    function asSingletonArray(bytes32 from) internal pure returns (bytes32[] memory array_) {
+    function asSingletonArray(bytes32 from_) internal pure returns (bytes32[] memory array_) {
         array_ = new bytes32[](1);
-        array_[0] = from;
+        array_[0] = from_;
     }
 }
