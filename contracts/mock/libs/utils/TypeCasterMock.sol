f5d0993014315d96ff1ea243a754cc2ccbb7c4b0
aritkulova <94910987+aritkulova@users.noreply.github.com>
2023-07-17 20:46:48 +0300

    ArrayConverter added (#50)
    * ArrayConverter added
    
    Coverage 100%
    
    * Updated ArrayConverter
    
    Added assembly for better performance
    
    * Update ArrayConverter.sol
    
    * cleaned up
    
    ---------
    
    Co-authored-by: Artem Chystiakov <artem.ch31@gmail.com>

--- contracts/mock/libs/utils/TypeCasterMock.sol
+++ contracts/mock/libs/utils/TypeCasterMock.sol
@@ -50,6 +50,10 @@ contract TypeCasterMock {
         return from_.asSingletonArray();
     }
 
+    function asSingletonArrayFromBool(bool from_) external pure returns (bool[] memory) {
+        return from_.asSingletonArray();
+    }
+
     function asSingletonArrayFromString(
         string memory from_
     ) external pure returns (string[] memory) {
@@ -58,5 +62,76 @@ contract TypeCasterMock {
 
     function asSingletonArrayFromBytes32(bytes32 from_) external pure returns (bytes32[] memory) {
         return from_.asSingletonArray();
+    }
+
+    function testUint() external pure returns (bool) {
+        return
+            (1 == _testLength([uint256(1)].asDynamic())) &&
+            (2 == _testLength([uint256(1), 2].asDynamic())) &&
+            (3 == _testLength([uint256(2), 3, 2].asDynamic())) &&
+            (4 == _testLength([uint256(2), 4, 3, 2].asDynamic())) &&
+            (5 == _testLength([uint256(2), 3, 2, 1, 5].asDynamic()));
+    }
+
+    function testAddress() external pure returns (bool) {
+        return
+            (1 == _testLength([address(0)].asDynamic())) &&
+            (2 == _testLength([address(0), address(1)].asDynamic())) &&
+            (3 == _testLength([address(0), address(1), address(0)].asDynamic())) &&
+            (4 == _testLength([address(0), address(1), address(0), address(1)].asDynamic())) &&
+            (5 ==
+                _testLength(
+                    [address(0), address(1), address(0), address(1), address(0)].asDynamic()
+                ));
+    }
+
+    function testBool() external pure returns (bool) {
+        return
+            (1 == _testLength([true].asDynamic())) &&
+            (2 == _testLength([true, true].asDynamic())) &&
+            (3 == _testLength([true, true, true].asDynamic())) &&
+            (4 == _testLength([true, true, true, true].asDynamic())) &&
+            (5 == _testLength([true, true, true, true, true].asDynamic()));
+    }
+
+    function testString() external pure returns (bool) {
+        return
+            (1 == _testLength([""].asDynamic())) &&
+            (2 == _testLength(["", ""].asDynamic())) &&
+            (3 == _testLength(["", "", ""].asDynamic())) &&
+            (4 == _testLength(["", "", "", ""].asDynamic())) &&
+            (5 == _testLength(["", "", "", "", ""].asDynamic()));
+    }
+
+    function testBytes32() external pure returns (bool) {
+        return
+            (1 == _testLength([bytes32(0)].asDynamic())) &&
+            (2 == _testLength([bytes32(0), bytes32(0)].asDynamic())) &&
+            (3 == _testLength([bytes32(0), bytes32(0), bytes32(0)].asDynamic())) &&
+            (4 == _testLength([bytes32(0), bytes32(0), bytes32(0), bytes32(0)].asDynamic())) &&
+            (5 ==
+                _testLength(
+                    [bytes32(0), bytes32(0), bytes32(0), bytes32(0), bytes32(0)].asDynamic()
+                ));
+    }
+
+    function _testLength(uint256[] memory array_) internal pure returns (uint256) {
+        return array_.length;
+    }
+
+    function _testLength(address[] memory array_) internal pure returns (uint256) {
+        return array_.length;
+    }
+
+    function _testLength(bool[] memory array_) internal pure returns (uint256) {
+        return array_.length;
+    }
+
+    function _testLength(string[] memory array_) internal pure returns (uint256) {
+        return array_.length;
+    }
+
+    function _testLength(bytes32[] memory array_) internal pure returns (uint256) {
+        return array_.length;
     }
 }
