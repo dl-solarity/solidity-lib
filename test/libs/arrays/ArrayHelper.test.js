6a5c73528dd514d5baac3b774a136e0700f979f8
Artem Chystiakov <artem.ch31@gmail.com>
2023-08-10 14:43:55 +0300

    made storage binsearch

--- test/libs/arrays/ArrayHelper.test.js
+++ test/libs/arrays/ArrayHelper.test.js
@@ -24,6 +24,138 @@ describe("ArrayHelperMock", () => {
     mock = await ArrayHelperMock.new();
   });
 
+  describe("prefix sum array", () => {
+    function getArraySum(arr) {
+      return arr.reduce((prev, cur) => prev + cur, 0);
+    }
+
+    function countPrefixes(arr) {
+      return arr.map((e, idx) => getArraySum(arr.slice(0, idx + 1)));
+    }
+
+    const array = [0, 100, 50, 4, 34, 520, 4];
+
+    describe("bounds", () => {
+      const arbitraryArray = [10, 20, 30, 30, 30, 40, 50];
+      const singletonArray = [100];
+      const identicalElemsArray = [100, 100, 100, 100, 100];
+
+      describe("lowerBound", () => {
+        it("should find the correct indices in the arbitrary array", async () => {
+          await mock.setArray(arbitraryArray);
+
+          assert.equal((await mock.lowerBound(1)).toNumber(), 0);
+          assert.equal((await mock.lowerBound(10)).toNumber(), 0);
+          assert.equal((await mock.lowerBound(15)).toNumber(), 1);
+          assert.equal((await mock.lowerBound(20)).toNumber(), 1);
+          assert.equal((await mock.lowerBound(25)).toNumber(), 2);
+          assert.equal((await mock.lowerBound(30)).toNumber(), 2);
+          assert.equal((await mock.lowerBound(35)).toNumber(), 5);
+          assert.equal((await mock.lowerBound(40)).toNumber(), 5);
+          assert.equal((await mock.lowerBound(45)).toNumber(), 6);
+          assert.equal((await mock.lowerBound(50)).toNumber(), 6);
+          assert.equal((await mock.lowerBound(100)).toNumber(), 7);
+        });
+
+        it("should find the correct indices in the singleton array", async () => {
+          await mock.setArray(singletonArray);
+
+          assert.equal((await mock.lowerBound(1)).toNumber(), 0);
+          assert.equal((await mock.lowerBound(100)).toNumber(), 0);
+          assert.equal((await mock.lowerBound(150)).toNumber(), 1);
+        });
+
+        it("should find the correct indices in the identical elements array", async () => {
+          await mock.setArray(identicalElemsArray);
+
+          assert.equal((await mock.lowerBound(1)).toNumber(), 0);
+          assert.equal((await mock.lowerBound(100)).toNumber(), 0);
+          assert.equal((await mock.lowerBound(150)).toNumber(), 5);
+        });
+
+        it("should find the correct indices in the empty array", async () => {
+          assert.equal((await mock.lowerBound(100)).toNumber(), 0);
+        });
+      });
+
+      describe("upperBound", () => {
+        it("should find the correct indices in the arbitrary array", async () => {
+          await mock.setArray(arbitraryArray);
+
+          assert.equal((await mock.upperBound(1)).toNumber(), 0);
+          assert.equal((await mock.upperBound(10)).toNumber(), 1);
+          assert.equal((await mock.upperBound(15)).toNumber(), 1);
+          assert.equal((await mock.upperBound(20)).toNumber(), 2);
+          assert.equal((await mock.upperBound(25)).toNumber(), 2);
+          assert.equal((await mock.upperBound(30)).toNumber(), 5);
+          assert.equal((await mock.upperBound(35)).toNumber(), 5);
+          assert.equal((await mock.upperBound(40)).toNumber(), 6);
+          assert.equal((await mock.upperBound(45)).toNumber(), 6);
+          assert.equal((await mock.upperBound(50)).toNumber(), 7);
+          assert.equal((await mock.upperBound(100)).toNumber(), 7);
+        });
+
+        it("should find the correct indices in the singleton array", async () => {
+          await mock.setArray(singletonArray);
+
+          assert.equal((await mock.upperBound(1)).toNumber(), 0);
+          assert.equal((await mock.upperBound(100)).toNumber(), 1);
+          assert.equal((await mock.upperBound(150)).toNumber(), 1);
+        });
+
+        it("should find the correct indices in the identical elements array", async () => {
+          await mock.setArray(identicalElemsArray);
+
+          assert.equal((await mock.upperBound(1)).toNumber(), 0);
+          assert.equal((await mock.upperBound(100)).toNumber(), 5);
+          assert.equal((await mock.upperBound(150)).toNumber(), 5);
+        });
+
+        it("should find the correct indices in the empty array", async () => {
+          assert.equal((await mock.upperBound(100)).toNumber(), 0);
+        });
+      });
+    });
+
+    describe("getRangeSum", () => {
+      it("should get the range sum properly if all conditions are met", async () => {
+        await mock.setArray(await mock.countPrefixes(array));
+
+        for (let l = 0; l < array.length; l++) {
+          for (let r = l; r < array.length; r++) {
+            assert.equal((await mock.getRangeSum(l, r)).toNumber(), getArraySum(array.slice(l, r + 1)));
+          }
+        }
+      });
+
+      it("should revert if the first index is greater than the last one", async () => {
+        await mock.setArray(await mock.countPrefixes(array));
+
+        await truffleAssert.reverts(mock.getRangeSum(array.length - 1, 0), "ArrayHelper: wrong range");
+        await truffleAssert.reverts(mock.getRangeSum(1, 0), "ArrayHelper: wrong range");
+        await truffleAssert.reverts(mock.getRangeSum(2, 1), "ArrayHelper: wrong range");
+      });
+
+      it("should revert if one of the indexes is out of range", async () => {
+        await truffleAssert.reverts(mock.getRangeSum(0, 0));
+
+        await mock.setArray(await mock.countPrefixes(array));
+
+        await truffleAssert.reverts(mock.getRangeSum(0, array.length));
+      });
+    });
+
+    describe("countPrefixes", () => {
+      it("should compute prefix array properly", async () => {
+        assert.deepEqual(await mock.countPrefixes([]), []);
+        assert.deepEqual(
+          (await mock.countPrefixes(array)).map((e) => e.toNumber()),
+          countPrefixes(array)
+        );
+      });
+    });
+  });
+
   describe("reverse", async () => {
     it("should reverse uint array", async () => {
       const arr = await mock.reverseUint([1, 2, 3]);
@@ -140,119 +272,6 @@ describe("ArrayHelperMock", () => {
       await truffleAssert.reverts(mock.insertUint([1], 1, [2]));
       await truffleAssert.reverts(mock.insertAddress([], 0, [FIRST]));
       await truffleAssert.reverts(mock.insertString(["1", "2"], 2, ["1"]));
-    });
-  });
-
-  describe("prefix sum array", () => {
-    function getArraySum(arr) {
-      return arr.reduce((prev, cur) => prev + cur, 0);
-    }
-
-    function countPrefixes(arr) {
-      return arr.map((e, idx) => getArraySum(arr.slice(0, idx + 1)));
-    }
-
-    const array = [0, 100, 50, 4, 34, 520, 4];
-
-    describe("countPrefixes", () => {
-      it("should compute prefix array properly", async () => {
-        assert.deepEqual(await mock.countPrefixes([]), []);
-        assert.deepEqual(
-          (await mock.countPrefixes(array)).map((e) => e.toNumber()),
-          countPrefixes(array)
-        );
-      });
-    });
-
-    describe("getRangeSum", () => {
-      it("should get the range sum properly if all conditions are met", async () => {
-        for (let l = 0; l < array.length; l++) {
-          for (let r = l; r < array.length; r++) {
-            assert.equal((await mock.getRangeSum(array, l, r)).toNumber(), getArraySum(array.slice(l, r + 1)));
-          }
-        }
-      });
-
-      it("should revert if the first index is greater than the last one", async () => {
-        await truffleAssert.reverts(mock.getRangeSum(array, array.length - 1, 0), "ArrayHelper: wrong range");
-        await truffleAssert.reverts(mock.getRangeSum(array, 1, 0), "ArrayHelper: wrong range");
-        await truffleAssert.reverts(mock.getRangeSum(array, 2, 1), "ArrayHelper: wrong range");
-      });
-
-      it("should revert if one of the indexes is out of range", async () => {
-        await truffleAssert.reverts(mock.getRangeSum([], 0, 0));
-        await truffleAssert.reverts(mock.getRangeSum(array, 0, array.length));
-      });
-    });
-
-    describe("bounds", () => {
-      const arbitraryArray = [10, 20, 30, 30, 30, 40, 50];
-      const singletonArray = [100];
-      const identicalElemsArray = [100, 100, 100, 100, 100];
-
-      describe("lowerBound", () => {
-        it("should find the correct indices in the arbitrary array", async () => {
-          assert.equal((await mock.lowerBound(arbitraryArray, 1)).toNumber(), 0);
-          assert.equal((await mock.lowerBound(arbitraryArray, 10)).toNumber(), 0);
-          assert.equal((await mock.lowerBound(arbitraryArray, 15)).toNumber(), 1);
-          assert.equal((await mock.lowerBound(arbitraryArray, 20)).toNumber(), 1);
-          assert.equal((await mock.lowerBound(arbitraryArray, 25)).toNumber(), 2);
-          assert.equal((await mock.lowerBound(arbitraryArray, 30)).toNumber(), 2);
-          assert.equal((await mock.lowerBound(arbitraryArray, 35)).toNumber(), 5);
-          assert.equal((await mock.lowerBound(arbitraryArray, 40)).toNumber(), 5);
-          assert.equal((await mock.lowerBound(arbitraryArray, 45)).toNumber(), 6);
-          assert.equal((await mock.lowerBound(arbitraryArray, 50)).toNumber(), 6);
-          assert.equal((await mock.lowerBound(arbitraryArray, 100)).toNumber(), 7);
-        });
-
-        it("should find the correct indices in the singleton array", async () => {
-          assert.equal((await mock.lowerBound(singletonArray, 1)).toNumber(), 0);
-          assert.equal((await mock.lowerBound(singletonArray, 100)).toNumber(), 0);
-          assert.equal((await mock.lowerBound(singletonArray, 150)).toNumber(), 1);
-        });
-
-        it("should find the correct indices in the identical elements array", async () => {
-          assert.equal((await mock.lowerBound(identicalElemsArray, 1)).toNumber(), 0);
-          assert.equal((await mock.lowerBound(identicalElemsArray, 100)).toNumber(), 0);
-          assert.equal((await mock.lowerBound(identicalElemsArray, 150)).toNumber(), 5);
-        });
-
-        it("should find the correct indices in the empty array", async () => {
-          assert.equal((await mock.lowerBound([], 100)).toNumber(), 0);
-        });
-      });
-
-      describe("upperBound", () => {
-        it("should find the correct indices in the arbitrary array", async () => {
-          assert.equal((await mock.upperBound(arbitraryArray, 1)).toNumber(), 0);
-          assert.equal((await mock.upperBound(arbitraryArray, 10)).toNumber(), 1);
-          assert.equal((await mock.upperBound(arbitraryArray, 15)).toNumber(), 1);
-          assert.equal((await mock.upperBound(arbitraryArray, 20)).toNumber(), 2);
-          assert.equal((await mock.upperBound(arbitraryArray, 25)).toNumber(), 2);
-          assert.equal((await mock.upperBound(arbitraryArray, 30)).toNumber(), 5);
-          assert.equal((await mock.upperBound(arbitraryArray, 35)).toNumber(), 5);
-          assert.equal((await mock.upperBound(arbitraryArray, 40)).toNumber(), 6);
-          assert.equal((await mock.upperBound(arbitraryArray, 45)).toNumber(), 6);
-          assert.equal((await mock.upperBound(arbitraryArray, 50)).toNumber(), 7);
-          assert.equal((await mock.upperBound(arbitraryArray, 100)).toNumber(), 7);
-        });
-
-        it("should find the correct indices in the singleton array", async () => {
-          assert.equal((await mock.upperBound(singletonArray, 1)).toNumber(), 0);
-          assert.equal((await mock.upperBound(singletonArray, 100)).toNumber(), 1);
-          assert.equal((await mock.upperBound(singletonArray, 150)).toNumber(), 1);
-        });
-
-        it("should find the correct indices in the identical elements array", async () => {
-          assert.equal((await mock.upperBound(identicalElemsArray, 1)).toNumber(), 0);
-          assert.equal((await mock.upperBound(identicalElemsArray, 100)).toNumber(), 5);
-          assert.equal((await mock.upperBound(identicalElemsArray, 150)).toNumber(), 5);
-        });
-
-        it("should find the correct indices in the empty array", async () => {
-          assert.equal((await mock.upperBound([], 100)).toNumber(), 0);
-        });
-      });
     });
   });
 
