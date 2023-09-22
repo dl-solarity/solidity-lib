97ae37ceaab31494fa16a5092ac85dc4b5696074
Artjom Galaktionov <artjom.galaktionov@28stone.com>
2023-09-22 10:33:13 +0300

    Add ERC20 facet

---
+++ contracts/diamond/facets/ERC20Facet.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {LibDiamond} from "../libraries/LibDiamond.sol";
+import {SolidStateERC20} from "@solidstate/contracts/token/ERC20/SolidStateERC20.sol";
+
+contract ERC20Facet is SolidStateERC20 {}
