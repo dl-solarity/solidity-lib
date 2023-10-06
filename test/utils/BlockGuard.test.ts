9d5d6f38a434802b9e0e17938a1ef47af9880016
Artem Chystiakov <artem.ch31@gmail.com>
2023-10-06 16:43:31 +0300

    block guard tests

---
+++ test/utils/BlockGuard.test.ts
@@ -0,0 +1,77 @@
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { Reverter } from "@/test/helpers/reverter";
+
+import { BlockGuardMock } from "@ethers-v6";
+
+describe("BlockGuard", () => {
+  const reverter = new Reverter();
+
+  let FIRST: SignerWithAddress;
+  let SECOND: SignerWithAddress;
+
+  let mock: BlockGuardMock;
+
+  before("setup", async () => {
+    [FIRST, SECOND] = await ethers.getSigners();
+
+    const BlockGuardMock = await ethers.getContractFactory("BlockGuardMock");
+    mock = await BlockGuardMock.deploy();
+
+    await reverter.snapshot();
+  });
+
+  afterEach(reverter.revert);
+
+  describe("lockBlock", () => {
+    it("should return zero if the resource key hasn't been locked", async () => {
+      expect(await mock.getLatestLockBlock(await mock.LOCK_LOCK_RESOURCE(), FIRST)).to.equal(0n);
+    });
+
+    it("should return the current block if the resource key has been locked", async () => {
+      await mock.deposit();
+
+      expect(await mock.getLatestLockBlock(await mock.DEPOSIT_WITHDRAW_RESOURCE(), FIRST)).to.equal(
+        await time.latestBlock()
+      );
+
+      expect(await mock.getLatestLockBlock(await mock.DEPOSIT_WITHDRAW_RESOURCE(), SECOND)).to.equal(0n);
+    });
+  });
+
+  describe("checkBlock", () => {
+    it("should allow to call in different blocks", async () => {
+      await mock.deposit();
+
+      await mock.withdraw();
+    });
+
+    it("should disallow to call in the same block", async () => {
+      await expect(
+        mock.multicall([mock.interface.encodeFunctionData("deposit"), mock.interface.encodeFunctionData("withdraw")])
+      ).to.be.revertedWith("BlockGuard: locked");
+    });
+  });
+
+  describe("checkLockBlock", () => {
+    it("should allow to call in different blocks", async () => {
+      await mock.lock();
+
+      const blockNumber = await time.latestBlock();
+
+      expect(await mock.getLatestLockBlock(await mock.LOCK_LOCK_RESOURCE(), FIRST)).to.equal(blockNumber);
+
+      await mock.lock();
+
+      expect(await mock.getLatestLockBlock(await mock.LOCK_LOCK_RESOURCE(), FIRST)).to.equal(blockNumber + 1);
+    });
+
+    it("should disallow to call in the same block", async () => {
+      await expect(
+        mock.multicall([mock.interface.encodeFunctionData("lock"), mock.interface.encodeFunctionData("lock")])
+      ).to.be.revertedWith("BlockGuard: locked");
+    });
+  });
+});
