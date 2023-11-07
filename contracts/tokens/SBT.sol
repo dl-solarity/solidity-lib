c373d30377e53fd27cfb3bfc1bb251470d9260a4
VolodymyrZolotukhin <60932900+VolodymyrZolotukhin@users.noreply.github.com>
2023-11-07 12:25:24 +0200

    changed ownerOf to _tokenOwners in _burn (#78)
    * changed ownerOf to _tokenOwners in _burn
    
    * changed ownerOf to _tokenOwners in _mint
    
    * added internal ownerOf
    
    * added doc, fixed require statements
    
    * bumped version, returned _ownerOf() into _burn()

--- contracts/tokens/SBT.sol
+++ contracts/tokens/SBT.sol
@@ -61,7 +61,7 @@ abstract contract SBT is ISBT, ERC165Upgradeable {
      * @return true if `tokenId_` exists, false otherwise
      */
     function tokenExists(uint256 tokenId_) public view virtual override returns (bool) {
-        return ownerOf(tokenId_) != address(0);
+        return _ownerOf(tokenId_) != address(0);
     }
 
     /**
@@ -101,7 +101,7 @@ abstract contract SBT is ISBT, ERC165Upgradeable {
      * @return address of an owner or `address(0)` if token does not exist
      */
     function ownerOf(uint256 tokenId_) public view virtual override returns (address) {
-        return _tokenOwners[tokenId_];
+        return _ownerOf(tokenId_);
     }
 
     /**
@@ -172,7 +172,7 @@ abstract contract SBT is ISBT, ERC165Upgradeable {
      * @param tokenId_ the token to burn
      */
     function _burn(uint256 tokenId_) internal virtual {
-        address owner_ = ownerOf(tokenId_);
+        address owner_ = _ownerOf(tokenId_);
         require(owner_ != address(0), "SBT: token doesn't exist");
 
         _beforeTokenAction(address(0), tokenId_);
@@ -202,6 +202,15 @@ abstract contract SBT is ISBT, ERC165Upgradeable {
      */
     function _setBaseURI(string memory baseURI_) internal virtual {
         _baseURI = baseURI_;
+    }
+
+    /**
+     * @notice The function to get the owner of a token
+     * @param tokenId_ the token to get the owner of
+     * @return address of an owner or `address(0)` if token does not exist
+     */
+    function _ownerOf(uint256 tokenId_) internal view virtual returns (address) {
+        return _tokenOwners[tokenId_];
     }
 
     /**
