5fb4474cd07264c3f27d59d70f2538105499c6ef
dovgopoly <69435717+dovgopoly@users.noreply.github.com>
2024-02-24 15:17:41 +0200

    Diamond Access Control (#89)
    * init ac
    
    * added tests
    
    * added require
    
    * cosmetic

---
+++ contracts/diamond/access/DiamondAccessControl.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+import {DiamondAccessControlStorage, IAccessControl} from "./DiamondAccessControlStorage.sol";
+
+/**
+ * @notice The Diamond standard module
+ *
+ * This is modified version of OpenZeppelin's AccessControl contract to be used as a Storage contract
+ * by the Diamond Standard.
+ */
+abstract contract DiamondAccessControl is DiamondAccessControlStorage {
+    /**
+     * @dev Modifier that checks that an account has a specific role. Reverts
+     * with a standardized message including the required role.
+     *
+     * The format of the revert reason is given by the following regular expression:
+     *
+     *  /^AccessControl: account (0x[0-9a-f]{40}) is missing role (0x[0-9a-f]{64})$/
+     */
+    modifier onlyRole(bytes32 role_) {
+        _checkRole(role_);
+        _;
+    }
+
+    /**
+     * @notice Sets `DEFAULT_ADMIN_ROLE` to `msg.sender`
+     */
+    function __DiamondAccessControl_init()
+        internal
+        onlyInitializing(DIAMOND_ACCESS_CONTROL_STORAGE_SLOT)
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    /**
+     * @inheritdoc IAccessControl
+     */
+    function grantRole(
+        bytes32 role_,
+        address account_
+    ) public virtual override onlyRole(getRoleAdmin(role_)) {
+        _grantRole(role_, account_);
+    }
+
+    /**
+     * @inheritdoc IAccessControl
+     */
+    function revokeRole(
+        bytes32 role_,
+        address account_
+    ) public virtual override onlyRole(getRoleAdmin(role_)) {
+        _revokeRole(role_, account_);
+    }
+
+    /**
+     * @inheritdoc IAccessControl
+     */
+    function renounceRole(bytes32 role_, address account_) public virtual override {
+        require(account_ == msg.sender, "AccessControl: can only renounce roles for self");
+
+        _revokeRole(role_, account_);
+    }
+
+    /**
+     * @dev Sets `adminRole` as ``role``'s admin role.
+     *
+     * Emits a {RoleAdminChanged} event.
+     */
+    function _setRoleAdmin(bytes32 role_, bytes32 adminRole_) internal virtual {
+        bytes32 previousAdminRole_ = getRoleAdmin(role_);
+
+        _getAccessControlStorage().roles[role_].adminRole = adminRole_;
+
+        emit RoleAdminChanged(role_, previousAdminRole_, adminRole_);
+    }
+
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleGranted} event.
+     */
+    function _grantRole(bytes32 role_, address account_) internal virtual {
+        require(!hasRole(role_, account_), "AccessControl: role is granted");
+
+        _getAccessControlStorage().roles[role_].members[account_] = true;
+
+        emit RoleGranted(role_, account_, msg.sender);
+    }
+
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleRevoked} event.
+     */
+    function _revokeRole(bytes32 role_, address account_) internal virtual {
+        require(hasRole(role_, account_), "AccessControl: role is not granted");
+
+        _getAccessControlStorage().roles[role_].members[account_] = false;
+
+        emit RoleRevoked(role_, account_, msg.sender);
+    }
+
+    /**
+     * @dev Revert with a standard message if `_msgSender()` is missing `role`.
+     * Overriding this function changes the behavior of the {onlyRole} modifier.
+     *
+     * Format of the revert message is described in {_checkRole}.
+     */
+    function _checkRole(bytes32 role_) internal view virtual {
+        _checkRole(role_, msg.sender);
+    }
+
+    /**_
+     * @dev Revert with a standard message if `account` is missing `role`.
+     *
+     * The format of the revert reason is given by the following regular expression:
+     *
+     *  /^AccessControl: account (0x[0-9a-f]{40}) is missing role (0x[0-9a-f]{64})$/
+     */
+    function _checkRole(bytes32 role_, address account_) internal view virtual {
+        if (!hasRole(role_, account_)) {
+            revert(
+                string(
+                    abi.encodePacked(
+                        "AccessControl: account ",
+                        Strings.toHexString(account_),
+                        " is missing role ",
+                        Strings.toHexString(uint256(role_), 32)
+                    )
+                )
+            );
+        }
+    }
+}
---
+++ test/diamond/DiamondAccessControl.ts
@@ -0,0 +1,161 @@
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Reverter } from "@/test/helpers/reverter";
+import { getSelectors, FacetAction } from "@/test/helpers/diamond-helper";
+import { ZERO_BYTES32 } from "@/scripts/utils/constants";
+
+import { OwnableDiamondMock, Diamond, DiamondAccessControlMock } from "@ethers-v6";
+
+describe("DiamondAccessControl", () => {
+  const reverter = new Reverter();
+
+  const ADMIN_ROLE = ZERO_BYTES32;
+  const AGENT_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000001";
+
+  let OWNER: SignerWithAddress;
+  let SECOND: SignerWithAddress;
+  let THIRD: SignerWithAddress;
+
+  let access: DiamondAccessControlMock;
+  let diamond: OwnableDiamondMock;
+
+  const hasRoleErrorMessage = async (account: SignerWithAddress, role: string) => {
+    return `AccessControl: account ${(await account.getAddress()).toLowerCase()} is missing role ${role}`;
+  };
+
+  before("setup", async () => {
+    [OWNER, SECOND, THIRD] = await ethers.getSigners();
+
+    const OwnableDiamond = await ethers.getContractFactory("OwnableDiamondMock");
+    const DiamondAccessControlMock = await ethers.getContractFactory("DiamondAccessControlMock");
+
+    diamond = await OwnableDiamond.deploy();
+    access = await DiamondAccessControlMock.deploy();
+
+    const facets: Diamond.FacetStruct[] = [
+      {
+        facetAddress: await access.getAddress(),
+        action: FacetAction.Add,
+        functionSelectors: getSelectors(access.interface),
+      },
+    ];
+
+    await diamond.diamondCutShort(facets);
+
+    access = <DiamondAccessControlMock>DiamondAccessControlMock.attach(await diamond.getAddress());
+
+    await access.__DiamondAccessControlMock_init();
+
+    await reverter.snapshot();
+  });
+
+  afterEach(reverter.revert);
+
+  describe("access", () => {
+    it("should initialize only once", async () => {
+      await expect(access.__DiamondAccessControlMock_init()).to.be.revertedWith(
+        "Initializable: contract is already initialized",
+      );
+    });
+
+    it("should initialize only by top level contract", async () => {
+      await expect(access.__DiamondAccessControlDirect_init()).to.be.revertedWith(
+        "Initializable: contract is not initializing",
+      );
+    });
+  });
+
+  describe("DiamondAccessControl functions", () => {
+    describe("grantRole", async () => {
+      it("should not grant role if not admin", async () => {
+        await expect(access.connect(SECOND).grantRole(AGENT_ROLE, SECOND)).to.be.revertedWith(
+          await hasRoleErrorMessage(SECOND, ADMIN_ROLE),
+        );
+      });
+
+      it("should not grant role if it's granted", async () => {
+        await access.grantRole(AGENT_ROLE, SECOND);
+
+        await expect(access.grantRole(AGENT_ROLE, SECOND)).to.be.revertedWith("AccessControl: role is granted");
+      });
+
+      it("should grant role if all conditions are met", async () => {
+        await access.grantRole(AGENT_ROLE, SECOND);
+
+        expect(await access.hasRole(AGENT_ROLE, SECOND)).to.be.true;
+      });
+    });
+
+    describe("revokeRole", async () => {
+      beforeEach(async () => {
+        await access.grantRole(AGENT_ROLE, SECOND);
+      });
+
+      it("should not revoke role if not admin", async () => {
+        await expect(access.connect(SECOND).revokeRole(AGENT_ROLE, SECOND)).to.be.revertedWith(
+          await hasRoleErrorMessage(SECOND, ADMIN_ROLE),
+        );
+      });
+
+      it("should not revoke role if it's not granted", async () => {
+        await access.revokeRole(AGENT_ROLE, SECOND);
+
+        await expect(access.revokeRole(AGENT_ROLE, SECOND)).to.be.revertedWith("AccessControl: role is not granted");
+      });
+
+      it("should revoke role if all conditions are met", async () => {
+        await access.revokeRole(AGENT_ROLE, SECOND);
+
+        expect(await access.hasRole(AGENT_ROLE, SECOND)).to.be.false;
+      });
+    });
+
+    describe("renounceRole", async () => {
+      beforeEach(async () => {
+        await access.grantRole(AGENT_ROLE, SECOND);
+      });
+
+      it("should not renounce role if not self", async () => {
+        await expect(access.renounceRole(AGENT_ROLE, SECOND)).to.be.revertedWith(
+          "AccessControl: can only renounce roles for self",
+        );
+      });
+
+      it("should renounce role if all conditions are met", async () => {
+        await access.connect(SECOND).renounceRole(AGENT_ROLE, SECOND);
+
+        expect(await access.hasRole(AGENT_ROLE, SECOND)).to.be.false;
+      });
+    });
+
+    describe("setAdminRole", async () => {
+      beforeEach(async () => {
+        await access.grantRole(AGENT_ROLE, SECOND);
+        await access.setRoleAdmin(AGENT_ROLE, AGENT_ROLE);
+      });
+
+      it("should not grant role if not admin", async () => {
+        await expect(access.grantRole(AGENT_ROLE, THIRD)).to.be.revertedWith(
+          await hasRoleErrorMessage(OWNER, AGENT_ROLE),
+        );
+      });
+
+      it("should grant role if all conditions are met", async () => {
+        await access.connect(SECOND).grantRole(AGENT_ROLE, THIRD);
+
+        expect(await access.hasRole(AGENT_ROLE, THIRD)).to.be.true;
+      });
+    });
+  });
+
+  describe("getters", () => {
+    it("should return base data", async () => {
+      expect(await access.DEFAULT_ADMIN_ROLE()).to.equal(ADMIN_ROLE);
+      expect(await access.AGENT_ROLE()).to.equal(AGENT_ROLE);
+      expect(await access.hasRole(ADMIN_ROLE, OWNER)).to.be.true;
+      expect(await access.hasRole(ADMIN_ROLE, SECOND)).to.be.false;
+      expect(await access.getRoleAdmin(AGENT_ROLE)).to.equal(ADMIN_ROLE);
+    });
+  });
+});
