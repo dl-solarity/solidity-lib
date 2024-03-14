316784227dc91bb689c2283bfac91ba7d105f0b6
dovgopoly <idovgopoly@gmail.com>
2024-03-14 16:53:46 +0200

    added strictAdd & strictRemove

--- test/libs/arrays/SetHelper.test.ts
+++ test/libs/arrays/SetHelper.test.ts
@@ -45,6 +45,40 @@ describe("SetHelper", () => {
     });
   });
 
+  describe("strictAdd", () => {
+    it("should not strict add to address set if element already exists", async () => {
+      await expect(mock.strictAddToAddressSet([FIRST.address, FIRST.address])).to.be.revertedWith(
+        "SetHelper: element already exists",
+      );
+    });
+
+    it("should not strict add to uint set if element already exists", async () => {
+      await expect(mock.strictAddToUintSet([1, 1])).to.be.revertedWith("SetHelper: element already exists");
+    });
+
+    it("should strict add to string set if element already exists", async () => {
+      await expect(mock.strictAddToStringSet(["1", "1"])).to.be.revertedWith("SetHelper: element already exists");
+    });
+
+    it("should strict add to address set", async () => {
+      await mock.strictAddToAddressSet([FIRST.address, SECOND.address]);
+
+      expect(await mock.getAddressSet()).to.deep.equal([FIRST.address, SECOND.address]);
+    });
+
+    it("should strict add to uint set", async () => {
+      await mock.strictAddToUintSet([1]);
+
+      expect(await mock.getUintSet()).to.deep.equal([1n]);
+    });
+
+    it("should strict add to string set", async () => {
+      await mock.strictAddToStringSet(["1", "2", "3"]);
+
+      expect(await mock.getStringSet()).to.deep.equal(["1", "2", "3"]);
+    });
+  });
+
   describe("remove", () => {
     it("should remove from address set", async () => {
       await mock.addToAddressSet([FIRST.address, SECOND.address]);
@@ -63,6 +97,49 @@ describe("SetHelper", () => {
     it("should remove from string set", async () => {
       await mock.addToStringSet(["1", "2", "3"]);
       await mock.removeFromStringSet(["1", "4"]);
+
+      expect(await mock.getStringSet()).to.deep.equal(["3", "2"]);
+    });
+  });
+
+  describe("remove", () => {
+    it("should not strict remove from address set if no such element", async () => {
+      await mock.strictAddToAddressSet([FIRST.address, SECOND.address]);
+
+      await expect(mock.strictRemoveFromAddressSet([SECOND.address, SECOND.address])).to.be.revertedWith(
+        "SetHelper: no such element",
+      );
+    });
+
+    it("should not strict remove from uint set if no such element", async () => {
+      await mock.strictAddToUintSet([1]);
+
+      await expect(mock.strictRemoveFromUintSet([1, 1])).to.be.revertedWith("SetHelper: no such element");
+    });
+
+    it("should not strict remove from string set if no such element", async () => {
+      await mock.strictAddToStringSet(["1", "2", "3"]);
+
+      await expect(mock.strictRemoveFromStringSet(["1", "1"])).to.be.revertedWith("SetHelper: no such element");
+    });
+
+    it("should strict remove from address set", async () => {
+      await mock.strictAddToAddressSet([FIRST.address, SECOND.address]);
+      await mock.strictRemoveFromAddressSet([SECOND.address]);
+
+      expect(await mock.getAddressSet()).to.deep.equal([FIRST.address]);
+    });
+
+    it("should strict remove from uint set", async () => {
+      await mock.strictAddToUintSet([1]);
+      await mock.strictRemoveFromUintSet([1]);
+
+      expect(await mock.getUintSet()).to.deep.equal([]);
+    });
+
+    it("should strict remove from string set", async () => {
+      await mock.strictAddToStringSet(["1", "2", "3"]);
+      await mock.strictRemoveFromStringSet(["1"]);
 
       expect(await mock.getStringSet()).to.deep.equal(["3", "2"]);
     });
