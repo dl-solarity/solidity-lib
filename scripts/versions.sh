bdee49deab63f8db7e00d9d6cdc55156978bde79
dovgopoly <idovgopoly@gmail.com>
2024-04-10 16:33:21 +0300

    style

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -6,14 +6,14 @@ registry_version=$(npm view $(node -p "require('./package.json').name") version)
 IFS='.' read -a local_version_array <<< $local_version
 IFS='.' read -a registry_version_array <<< $registry_version
 
-version_changed="NOT_CHANGED"
+version_changed="not_changed"
 
 if [[ ${local_version_array[0]} -gt ${registry_version_array[0]} ]]; then
-  version_changed="MAJOR"
+  version_changed="major"
 elif [[ ${local_version_array[1]} -gt ${registry_version_array[1]} ]]; then
-   version_changed="MINOR"
+   version_changed="minor"
 elif [[ ${local_version_array[2]} -gt ${registry_version_array[2]} ]]; then
-   version_changed="PATCH"
+   version_changed="patch"
 fi
 
 echo "::set-output name=local_version::$local_version"
