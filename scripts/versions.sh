7cdb0585405f211b9846b1f023a157d8a9fe20b4
dovgopoly <69435717+dovgopoly@users.noreply.github.com>
2024-04-10 19:21:36 +0300

    npm ci (#97)
    * added npm ci
    
    * release workflow
    
    * rm debug print
    
    * style
    
    * fixes

---
+++ scripts/versions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+local_version=$(node -p "require('./package.json').version")
+registry_version=$(npm view $(node -p "require('./package.json').name") version)
+
+IFS='.' read -a local_version_array <<< $local_version
+IFS='.' read -a registry_version_array <<< $registry_version
+
+version_changed="not_changed"
+
+if [[ ${local_version_array[0]} -gt ${registry_version_array[0]} ]]; then
+  version_changed="major"
+elif [[ ${local_version_array[1]} -gt ${registry_version_array[1]} ]]; then
+   version_changed="minor"
+elif [[ ${local_version_array[2]} -gt ${registry_version_array[2]} ]]; then
+   version_changed="patch"
+fi
+
+echo "::set-output name=local_version::$local_version"
+echo "::set-output name=version_changed::$version_changed"
