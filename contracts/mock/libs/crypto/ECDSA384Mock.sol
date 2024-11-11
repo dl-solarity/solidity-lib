05e84924710dcd6ccbf66f8f2d76667c0a3e9d2b
Artem Chystiakov <artem.ch31@gmail.com>
2024-11-11 15:16:57 +0200

    added ecdsa384 lib

---
+++ contracts/mock/libs/crypto/ECDSA384Mock.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {ECDSA384} from "../../../libs/crypto/ECDSA384.sol";
+
+contract ECDSA384Mock {
+    using ECDSA384 for *;
+
+    function verifySECP384r1(
+        bytes calldata message_,
+        bytes calldata signature_,
+        bytes calldata pubKey_
+    ) external view returns (bool) {
+        ECDSA384.Parameters memory curveParams_ = ECDSA384.Parameters({
+            a: hex"fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000fffffffc",
+            b: hex"b3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef",
+            gx: hex"aa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7",
+            gy: hex"3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f",
+            p: hex"fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000ffffffff",
+            n: hex"ffffffffffffffffffffffffffffffffffffffffffffffffc7634d81f4372ddf581a0db248b0a77aecec196accc52973",
+            lowSmax: hex"7fffffffffffffffffffffffffffffffffffffffffffffffe3b1a6c0fa1b96efac0d06d9245853bd76760cb5666294b9"
+        });
+
+        return curveParams_.verify(abi.encodePacked(sha256(message_)), signature_, pubKey_);
+    }
+}
