f2c046e1dbb3fbf85c4395b35b142d69c2a86041
aritkulova <aritkulova.yuliia@gmail.com>
2024-11-13 17:31:26 +0200

    changed compiler version to 0.8.22

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,7 +26,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.25",
+        version: "0.8.22",
         settings: {
           optimizer: {
             enabled: true,
