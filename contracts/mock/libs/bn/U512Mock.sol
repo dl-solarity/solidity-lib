b3cffd6623eaabddbb42eef69110936b5aa3ac1b
mllwchrry <mariia.zhvanko@gmail.com>
2025-01-23 19:15:41 +0200

    add tests for U512

--- contracts/mock/libs/bn/U512Mock.sol
+++ contracts/mock/libs/bn/U512Mock.sol
@@ -8,6 +8,241 @@ import {U512} from "../../../libs/bn/U512.sol";
 contract U512Mock {
     using U512 for *;
 
+    function copy(
+        uint256 u256_
+    )
+        external
+        view
+        returns (
+            uint512 pointerOriginal_,
+            uint512 pointerCopy_,
+            bytes memory valueOriginal_,
+            bytes memory valueCopy_
+        )
+    {
+        pointerOriginal_ = U512.fromUint256(u256_);
+        valueOriginal_ = U512.toBytes(pointerOriginal_);
+
+        pointerCopy_ = U512.copy(pointerOriginal_);
+        valueCopy_ = U512.toBytes(pointerCopy_);
+    }
+
+    function isNull(uint512 pointer_) external view returns (bool isNull_) {
+        return U512.isNull(pointer_);
+    }
+
+    function eq(bytes memory aBytes_, bytes memory bBytes_) external view returns (bool eq_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        return U512.eq(a_, b_);
+    }
+
+    function eqUint256(bytes memory aBytes_, uint256 u256_) external view returns (bool eq_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+
+        return U512.eqUint256(a_, u256_);
+    }
+
+    function cmp(bytes memory aBytes_, bytes memory bBytes_) external view returns (int256) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        return U512.cmp(a_, b_);
+    }
+
+    function mod(
+        bytes memory aBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        return U512.mod(call_, a_, m_).toBytes();
+    }
+
+    function modAssign(
+        bytes memory aBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.modAssign(call_, a_, m_);
+
+        return a_.toBytes();
+    }
+
+    function modAssignTo(
+        bytes memory aBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.modAssignTo(call_, a_, m_, to_);
+
+        return to_.toBytes();
+    }
+
+    function modinv(
+        bytes memory aBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        return U512.modinv(call_, a_, m_).toBytes();
+    }
+
+    function modinvAssign(
+        bytes memory aBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.modinvAssign(call_, a_, m_);
+
+        return a_.toBytes();
+    }
+
+    function modinvAssignTo(
+        bytes memory aBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.modinvAssignTo(call_, a_, m_, to_);
+
+        return to_.toBytes();
+    }
+
+    function add(
+        bytes memory aBytes_,
+        bytes memory bBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        return U512.add(a_, b_).toBytes();
+    }
+
+    function addAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        U512.addAssign(a_, b_);
+
+        return a_.toBytes();
+    }
+
+    function addAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.addAssignTo(a_, b_, to_);
+
+        return to_.toBytes();
+    }
+
+    function sub(
+        bytes memory aBytes_,
+        bytes memory bBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        return U512.sub(a_, b_).toBytes();
+    }
+
+    function subAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        U512.subAssign(a_, b_);
+
+        return a_.toBytes();
+    }
+
+    function subAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.subAssignTo(a_, b_, to_);
+
+        return to_.toBytes();
+    }
+
+    function mul(
+        bytes memory aBytes_,
+        bytes memory bBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        return U512.mul(a_, b_).toBytes();
+    }
+
+    function mulAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+
+        U512.mulAssign(a_, b_);
+
+        return a_.toBytes();
+    }
+
+    function mulAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.mulAssignTo(a_, b_, to_);
+
+        return to_.toBytes();
+    }
+
     function modadd(
         bytes memory aBytes_,
         bytes memory bBytes_,
@@ -20,6 +255,88 @@ contract U512Mock {
         uint512 m_ = U512.fromBytes(mBytes_);
 
         return U512.modadd(call_, a_, b_, m_).toBytes();
+    }
+
+    function modaddAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.modaddAssign(call_, a_, b_, m_);
+
+        return a_.toBytes();
+    }
+
+    function modaddAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.modaddAssignTo(call_, a_, b_, m_, to_);
+
+        return to_.toBytes();
+    }
+
+    function redadd(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        return U512.redadd(call_, a_, b_, m_).toBytes();
+    }
+
+    function redaddAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.redaddAssign(call_, a_, b_, m_);
+
+        return a_.toBytes();
+    }
+
+    function redaddAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.redaddAssignTo(call_, a_, b_, m_, to_);
+
+        return to_.toBytes();
     }
 
     function modsub(
@@ -36,6 +353,88 @@ contract U512Mock {
         return U512.modsub(call_, a_, b_, m_).toBytes();
     }
 
+    function modsubAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.modsubAssign(call_, a_, b_, m_);
+
+        return a_.toBytes();
+    }
+
+    function modsubAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.modsubAssignTo(call_, a_, b_, m_, to_);
+
+        return to_.toBytes();
+    }
+
+    function redsub(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        return U512.redsub(call_, a_, b_, m_).toBytes();
+    }
+
+    function redsubAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.redsubAssign(call_, a_, b_, m_);
+
+        return a_.toBytes();
+    }
+
+    function redsubAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.redsubAssignTo(call_, a_, b_, m_, to_);
+
+        return to_.toBytes();
+    }
+
     function modmul(
         bytes memory aBytes_,
         bytes memory bBytes_,
@@ -48,6 +447,40 @@ contract U512Mock {
         uint512 m_ = U512.fromBytes(mBytes_);
 
         return U512.modmul(call_, a_, b_, m_).toBytes();
+    }
+
+    function modmulAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.modmulAssign(call_, a_, b_, m_);
+
+        return a_.toBytes();
+    }
+
+    function modmulAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.modmulAssignTo(call_, a_, b_, m_, to_);
+
+        return to_.toBytes();
     }
 
     function modexp(
@@ -64,6 +497,40 @@ contract U512Mock {
         return U512.modexp(call_, a_, b_, m_).toBytes();
     }
 
+    function modexpAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.modexpAssign(call_, a_, b_, m_);
+
+        return a_.toBytes();
+    }
+
+    function modexpAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.modexpAssignTo(call_, a_, b_, m_, to_);
+
+        return to_.toBytes();
+    }
+
     function moddiv(
         bytes memory aBytes_,
         bytes memory bBytes_,
@@ -76,5 +543,39 @@ contract U512Mock {
         uint512 m_ = U512.fromBytes(mBytes_);
 
         return U512.moddiv(call_, a_, b_, m_).toBytes();
+    }
+
+    function moddivAssign(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+
+        U512.moddivAssign(call_, a_, b_, m_);
+
+        return a_.toBytes();
+    }
+
+    function moddivAssignTo(
+        bytes memory aBytes_,
+        bytes memory bBytes_,
+        bytes memory mBytes_,
+        bytes memory toBytes_
+    ) external view returns (bytes memory rBytes_) {
+        call call_ = U512.initCall();
+
+        uint512 a_ = U512.fromBytes(aBytes_);
+        uint512 b_ = U512.fromBytes(bBytes_);
+        uint512 m_ = U512.fromBytes(mBytes_);
+        uint512 to_ = U512.fromBytes(toBytes_);
+
+        U512.moddivAssignTo(call_, a_, b_, m_, to_);
+
+        return to_.toBytes();
     }
 }
--- test/libs/bn/U512.test.ts
+++ test/libs/bn/U512.test.ts
@@ -4,8 +4,10 @@ import { Reverter } from "@/test/helpers/reverter";
 
 import { U512Mock } from "@ethers-v6";
 
-describe("U512", () => {
+describe.only("U512", () => {
   const reverter = new Reverter();
+
+  const prime = 76884956397045344220809746629001649092737531784414529538755519063063536359079n;
 
   let u512: U512Mock;
 
@@ -15,6 +17,47 @@ describe("U512", () => {
 
   function toBytes(value: bigint): string {
     return "0x" + value.toString(16).padStart(128, "0");
+  }
+
+  function mod(a: string, m: string): string {
+    return toBytes(ethers.toBigInt(a) % ethers.toBigInt(m));
+  }
+
+  function add(a: string, b: string): string {
+    const maxUint512 = BigInt(1) << BigInt(512);
+
+    const aBigInt = ethers.toBigInt(a);
+    const bBigInt = ethers.toBigInt(b);
+
+    const result = (aBigInt + bBigInt) % maxUint512;
+
+    return toBytes(result);
+  }
+
+  function sub(a: string, b: string): string {
+    const maxUint512 = BigInt(1) << BigInt(512);
+
+    const aBigInt = ethers.toBigInt(a);
+    const bBigInt = ethers.toBigInt(b);
+
+    let result = (aBigInt - bBigInt) % maxUint512;
+
+    if (result < 0) {
+      result += maxUint512;
+    }
+
+    return toBytes(result);
+  }
+
+  function mul(a: string, b: string): string {
+    const maxUint512 = BigInt(1) << BigInt(512);
+
+    const aBigInt = ethers.toBigInt(a);
+    const bBigInt = ethers.toBigInt(b);
+
+    const result = (aBigInt * bBigInt) % maxUint512;
+
+    return toBytes(result);
   }
 
   function modadd(a: string, b: string, m: string): string {
@@ -27,6 +70,34 @@ describe("U512", () => {
 
   function modexp(a: string, b: string, m: string): string {
     return toBytes(ethers.toBigInt(a) ** ethers.toBigInt(b) % ethers.toBigInt(m));
+  }
+
+  function modinv(a: string, m: string): string {
+    const aBigInt = ethers.toBigInt(a);
+    const mBigInt = ethers.toBigInt(m);
+
+    if (aBigInt <= 0n || mBigInt <= 0n) {
+      throw new Error("Inputs must be positive integers.");
+    }
+
+    let [t, newT] = [0n, 1n];
+    let [r, newR] = [mBigInt, aBigInt];
+
+    while (newR !== 0n) {
+      const quotient = r / newR;
+      [t, newT] = [newT, t - quotient * newT];
+      [r, newR] = [newR, r - quotient * newR];
+    }
+
+    if (r > 1n) {
+      throw new Error("No modular inverse exists.");
+    }
+
+    if (t < 0n) {
+      t += mBigInt;
+    }
+
+    return toBytes(t);
   }
 
   function modsub(a: string, b: string, m: string): string {
@@ -47,13 +118,125 @@ describe("U512", () => {
 
   afterEach(reverter.revert);
 
+  it("copy test", async () => {
+    const [pointerOriginal, pointerCopy, valueOriginal, valueCopy] = await u512.copy(prime);
+
+    expect(pointerOriginal).to.be.lessThan(pointerCopy);
+    expect(valueOriginal).to.be.equal(valueCopy);
+  });
+
+  it("isNull test", async () => {
+    expect(await u512.isNull(0)).to.be.true;
+    expect(await u512.isNull(64)).to.be.false;
+  });
+
+  it("eq test", async () => {
+    expect(await u512.eq(toBytes(1020n), toBytes(1002n))).to.be.false;
+    expect(await u512.eq(toBytes(200n), toBytes(200n))).to.be.true;
+    expect(await u512.eq("0x00", "0x00")).to.be.true;
+  });
+
+  it("eqUint256 test", async () => {
+    expect(await u512.eqUint256(toBytes(1020n), 1002n)).to.be.false;
+    expect(await u512.eqUint256(toBytes(200n), 200n)).to.be.true;
+    expect(await u512.eqUint256("0x00", 0)).to.be.true;
+  });
+
+  it("cmp test", async () => {
+    expect(await u512.cmp(toBytes(705493n), toBytes(705492n))).to.be.equal(1);
+    expect(await u512.cmp(toBytes(1n), "0x00")).to.be.equal(1);
+    expect(await u512.cmp(toBytes(775n), toBytes(775n))).to.be.equal(0);
+    expect(await u512.cmp("0x00", "0x00")).to.be.equal(0);
+    expect(await u512.cmp(toBytes(380n), toBytes(400n))).to.be.equal(-1);
+    expect(await u512.cmp("0x00", toBytes(12n))).to.be.equal(-1);
+  });
+
+  it("mod test", async () => {
+    for (let i = 0; i < 100; ++i) {
+      const a = randomU512();
+      const m = randomU512();
+      const to = randomU512();
+
+      expect(await u512.mod(a, m)).to.be.equal(mod(a, m));
+      expect(await u512.modAssign(a, m)).to.be.equal(mod(a, m));
+      expect(await u512.modAssignTo(a, m, to)).to.be.equal(mod(a, m));
+    }
+  });
+
+  it("modinv test", async () => {
+    const m = toBytes(prime);
+
+    for (let i = 0; i < 100; ++i) {
+      const a = randomU512();
+      const to = randomU512();
+
+      expect(await u512.modinv(a, m)).to.be.equal(modinv(a, m));
+      expect(await u512.modinvAssign(a, m)).to.be.equal(modinv(a, m));
+      expect(await u512.modinvAssignTo(a, m, to)).to.be.equal(modinv(a, m));
+    }
+  });
+
+  it("add test", async () => {
+    for (let i = 0; i < 100; ++i) {
+      const a = randomU512();
+      const b = randomU512();
+      const to = randomU512();
+
+      expect(await u512.add(a, b)).to.be.equal(add(a, b));
+      expect(await u512.addAssign(a, b)).to.be.equal(add(a, b));
+      expect(await u512.addAssignTo(a, b, to)).to.be.equal(add(a, b));
+    }
+  });
+
+  it("sub test", async () => {
+    for (let i = 0; i < 100; ++i) {
+      const a = randomU512();
+      const b = randomU512();
+      const to = randomU512();
+
+      expect(await u512.sub(a, b)).to.be.equal(sub(a, b));
+      expect(await u512.subAssign(a, b)).to.be.equal(sub(a, b));
+      expect(await u512.subAssignTo(a, b, to)).to.be.equal(sub(a, b));
+    }
+  });
+
+  it("mul test", async () => {
+    for (let i = 0; i < 100; ++i) {
+      const a = randomU512();
+      const b = randomU512();
+      const to = randomU512();
+
+      expect(await u512.mul(a, b)).to.be.equal(mul(a, b));
+      expect(await u512.mulAssign(a, b)).to.be.equal(mul(a, b));
+      expect(await u512.mulAssignTo(a, b, to)).to.be.equal(mul(a, b));
+    }
+  });
+
   it("modadd test", async () => {
     for (let i = 0; i < 100; ++i) {
       const a = randomU512();
       const b = randomU512();
       const m = randomU512();
+      const to = randomU512();
 
       expect(await u512.modadd(a, b, m)).to.equal(modadd(a, b, m));
+      expect(await u512.modaddAssign(a, b, m)).to.equal(modadd(a, b, m));
+      expect(await u512.modaddAssignTo(a, b, m, to)).to.equal(modadd(a, b, m));
+    }
+  });
+
+  it("redadd test", async () => {
+    for (let i = 0; i < 100; ++i) {
+      const m = randomU512();
+
+      const a = mod(randomU512(), m);
+      const b = mod(randomU512(), m);
+
+      const to = randomU512();
+
+      expect(await u512.redadd(a, b, m)).to.equal(modadd(a, b, m));
+      expect(await u512.redaddAssign(a, b, m)).to.equal(modadd(a, b, m));
+      expect(await u512.redaddAssignTo(a, b, m, to)).to.equal(modadd(a, b, m));
     }
   });
 
@@ -62,8 +245,11 @@ describe("U512", () => {
       const a = randomU512();
       const b = randomU512();
       const m = randomU512();
+      const to = randomU512();
 
       expect(await u512.modmul(a, b, m)).to.equal(modmul(a, b, m));
+      expect(await u512.modmulAssign(a, b, m)).to.equal(modmul(a, b, m));
+      expect(await u512.modmulAssignTo(a, b, m, to)).to.equal(modmul(a, b, m));
     }
   });
 
@@ -72,8 +258,26 @@ describe("U512", () => {
       const a = randomU512();
       const b = randomU512();
       const m = randomU512();
+      const to = randomU512();
 
       expect(await u512.modsub(a, b, m)).to.equal(modsub(a, b, m));
+      expect(await u512.modsubAssign(a, b, m)).to.equal(modsub(a, b, m));
+      expect(await u512.modsubAssignTo(a, b, m, to)).to.equal(modsub(a, b, m));
+    }
+  });
+
+  it("redsub test", async () => {
+    for (let i = 0; i < 100; ++i) {
+      const m = randomU512();
+
+      const a = mod(randomU512(), m);
+      const b = mod(randomU512(), m);
+
+      const to = randomU512();
+
+      expect(await u512.redsub(a, b, m)).to.equal(modsub(a, b, m));
+      expect(await u512.redsubAssign(a, b, m)).to.equal(modsub(a, b, m));
+      expect(await u512.redsubAssignTo(a, b, m, to)).to.equal(modsub(a, b, m));
     }
   });
 
@@ -82,18 +286,26 @@ describe("U512", () => {
       const a = randomU512();
       const b = toBytes(100n);
       const m = randomU512();
+      const to = randomU512();
 
       expect(await u512.modexp(a, b, m)).to.equal(modexp(a, b, m));
+      expect(await u512.modexpAssign(a, b, m)).to.equal(modexp(a, b, m));
+      expect(await u512.modexpAssignTo(a, b, m, to)).to.equal(modexp(a, b, m));
     }
   });
 
   it("moddiv test", async () => {
+    const m = toBytes(prime);
+
     const a = toBytes(779149564533142355434093157610126726613246737199n);
     const b = toBytes(29118654464229156312755475164902924590603964377702716942232927993582928167089n);
-    const m = toBytes(76884956397045344220809746629001649092737531784414529538755519063063536359079n);
+
+    const to = randomU512();
 
     const expected = toBytes(30823410400962253491978005949535646087432096635784775122170630924100507445065n);
 
     expect(await u512.moddiv(a, b, m)).to.equal(expected);
+    expect(await u512.moddivAssign(a, b, m)).to.equal(expected);
+    expect(await u512.moddivAssignTo(a, b, m, to)).to.equal(expected);
   });
 });
