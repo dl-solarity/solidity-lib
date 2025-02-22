acccc7d4c9bc2cc6b5dbd97d276d60b567e81588
Noisy <125606576+donatik27@users.noreply.github.com>
2025-02-22 22:09:37 +0100

    Update PermanentOwnable.sol

--- contracts/access/PermanentOwnable.sol
+++ contracts/access/PermanentOwnable.sol
@@ -10,7 +10,7 @@ pragma solidity ^0.8.4;
  * The owner is set to the address provided by the deployer. The ownership cannot be further changed.
  *
  * This module will make available the modifier `onlyOwner`, which can be applied
- * to your functions to restrict their use to the owners.
+ * to your functions to restrict their use to the owner.
  */
 abstract contract PermanentOwnable {
     address private immutable _OWNER;
@@ -28,7 +28,7 @@ abstract contract PermanentOwnable {
      * @param owner_ the address of the permanent owner.
      */
     constructor(address owner_) {
-        require(owner_ != address(0), "PermanentOwnable: zero address can not be the owner");
+        require(owner_ != address(0), "PermanentOwnable: zero address cannot be the owner");
 
         _OWNER = owner_;
     }
