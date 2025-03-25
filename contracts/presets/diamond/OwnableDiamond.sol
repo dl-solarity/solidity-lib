cd205a136f7532afa8b46a7ecad96c82031ee56c
aritkulova <94910987+aritkulova@users.noreply.github.com>
2025-03-25 13:52:06 +0200

    fixed typo

--- contracts/presets/diamond/OwnableDiamond.sol
+++ contracts/presets/diamond/OwnableDiamond.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 import {Diamond} from "../../diamond/Diamond.sol";
 
