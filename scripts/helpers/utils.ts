195f08030135f9382813715cea05715b66b0be60
Kyryl R <kyryl.ryabov@gmail.com>
2025-08-12 16:53:57 +0300

    Fixed typo

--- scripts/helpers/utils.ts
+++ scripts/helpers/utils.ts
@@ -95,7 +95,7 @@ export function validateReleaseTopSection({
   const normalized = String(level).toLowerCase() as Level;
   if (!(pkgIsRc ? allowedWhenRc.has(normalized) : allowedWhenNotRc.has(normalized))) {
     if (pkgIsRc) {
-      throw new Error("Top H2 tag must be one of rc|nonce|release when current version is an RC");
+      throw new Error("Top H2 tag must be one of rc|none|release when current version is an RC");
     } else {
       throw new Error("Top H2 tag must be one of patch|minor|major|none|patch-rc|minor-rc|major-rc when not in RC");
     }
