a6eacbc4105cadc0e6ae18cebbbca8726fa42604
Everton Fraga <effraga@amazon.com>
2025-09-04 12:36:24 -0400

    Re-enabling tests for ECDSA libs

--- test/libs/crypto/ECDSA384.test.ts
+++ test/libs/crypto/ECDSA384.test.ts
@@ -30,7 +30,7 @@ function modifyRight(value: string, modifier: string): string {
   return newSignature;
 }
 
-describe.skip("ECDSA384", () => {
+describe("ECDSA384", () => {
   const reverter = new Reverter();
 
   let ecdsa384: ECDSA384Mock;
